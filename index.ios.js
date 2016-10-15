6cd4713bbe6772d732bc3c2ac924286d48954e22
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-15 14:26:46 -0700

    Add react-native-base

--- index.ios.js
+++ index.ios.js
@@ -12,21 +12,36 @@ import {
   View
 } from 'react-native';
 
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon
+} from 'native-base';
+
 export default class CheeseNerdMobile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Container>
+        <Header>
+          <Title>Header</Title>
+        </Header>
+
+        <Content>
+        </Content>
+
+        <Footer>
+          <FooterTab>
+            <Button transparent>
+              <Icon name='ios-call' />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
