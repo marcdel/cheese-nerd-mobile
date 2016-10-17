ce490b4d19e5dc7101f9c57d4007b1e11804b421
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-17 07:06:08 -0700

    Add clear search button.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -23,6 +23,7 @@ export default class SearchBar extends Component {
       <InputGroup>
         <Icon name='ios-search' />
         <Input value={this.props.query} onChangeText={(value) => this.props.textChanged(value)} placeholder='Search'/>
+        <Button transparent onPress={() => this.props.textChanged('')}><Icon name='ios-close' /></Button>
       </InputGroup>
     );
   }
