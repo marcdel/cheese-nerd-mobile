c6868f099eaf1813c81263eef1851563f44b071d
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-17 20:21:59 -0700

    Add active property to active tab.

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -17,13 +17,25 @@ export default class BottomNav extends Component {
   }
 
   render () {
+    const tab = this.props.tab;
+
     return (
       <FooterTab>
-        <Button onPress={() => this.props.tabChanged('myBoard')} transparent>My Board<Icon name='ios-restaurant' /></Button>
-        <Button onPress={() => this.props.tabChanged('search')} transparent>Search<Icon name='ios-search' /></Button>
-        <Button onPress={() => this.props.tabChanged('add')} transparent>Add<Icon name='ios-add-circle' /></Button>
-        <Button onPress={() => this.props.tabChanged('promo')} transparent>Promo<Icon name='ios-ribbon' /></Button>
-        <Button onPress={() => this.props.tabChanged('account')} transparent>Account<Icon name='ios-person' /></Button>
+        <Button active={tab === 'myBoard'} onPress={() => this.props.tabChanged('myBoard')}>
+          My Board<Icon name='ios-restaurant' />
+        </Button>
+        <Button active={tab === 'search'} onPress={() => this.props.tabChanged('search')}>
+          Search<Icon name='ios-search' />
+        </Button>
+        <Button active={tab === 'add'} onPress={() => this.props.tabChanged('add')}>
+          Add<Icon name='ios-add-circle' />
+        </Button>
+        <Button active={tab === 'promo'} onPress={() => this.props.tabChanged('promo')}>
+          Promo<Icon name='ios-ribbon' />
+        </Button>
+        <Button active={tab === 'account'} onPress={() => this.props.tabChanged('account')}>
+          Account<Icon name='ios-person' />
+        </Button>
       </FooterTab>
     );
   }
