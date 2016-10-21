966da22b5bde07e8faa988556b5ef88d38fba7a1
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-20 21:25:21 -0700

    Add examples of getting image, name and email.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {FBLoginManager} from 'react-native-facebook-login';
+import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 
 import {
   Container,
@@ -22,12 +22,42 @@ import { login } from '../actions/user';
 export class Login extends Component {
   constructor() {
     super();
+    this.loginPressed = this.loginPressed.bind(this);
+    this.getFbUserImage = this.getFbUserImage.bind(this);
+    this.getFbUserNameEmail = this.getFbUserNameEmail.bind(this);
+  }
+
+  getFbUserImage (userId, token) {
+    var api = `https://graph.facebook.com/v2.3/${userId}/picture?redirect=false&access_token=${token}`;
+
+    fetch(api)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log("Photo Url: ", responseData.data.url);
+      console.log("Photo height: ", responseData.data.height);
+      console.log("Photo width: ", responseData.data.width);
+    }).done();
+  }
+
+  getFbUserNameEmail (userId, token) {
+    var api = `https://graph.facebook.com/v2.3/${userId}?fields=name,email&access_token=${token}`;
+
+    fetch(api)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log("Date: ", responseData);
+      console.log("User name: ", responseData.name);
+      console.log("User email: ", responseData.email);
+    }).done();
   }
 
   loginPressed () {
+    let _this = this;
     FBLoginManager.loginWithPermissions(["email","public_profile","user_friends"], function(error, data){
       if (!error) {
         console.log("Login data: ", data);
+        _this.getFbUserImage(data.credentials.userId, data.credentials.token);
+        _this.getFbUserNameEmail(data.credentials.userId, data.credentials.token);
       } else {
         console.log("Error: ", error);
       }
@@ -45,6 +75,35 @@ export class Login extends Component {
         </Header>
 
         <Content style={{paddingTop: 250}}>
+          <FBLogin style={{ marginBottom: 10, }}
+            permissions={["email","public_profile","user_friends"]}
+            loginBehavior={FBLoginManager.LoginBehaviors.Native}
+            onLogin={function(data){
+              console.log("Logged in!");
+              console.log(data);
+            }}
+            onLogout={function(){
+              console.log("Logged out.");
+            }}
+            onLoginFound={function(data){
+              console.log("Existing login found.");
+              console.log(data);
+            }}
+            onLoginNotFound={function(){
+              console.log("No user logged in.");
+            }}
+            onError={function(data){
+              console.log("ERROR");
+              console.log(data);
+            }}
+            onCancel={function(){
+              console.log("User cancelled.");
+            }}
+            onPermissionsMissing={function(data){
+              console.log("Check permissions!");
+              console.log(data);
+            }}
+            />
           <Button onPress={() => this.loginPressed()} block>Log In</Button>
         </Content>
       </Container>
