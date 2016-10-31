f9b8c2b3cd0aa3f9413823632957fb2808867cfe
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-30 18:17:48 -0700

    Update promo tab.

--- src/containers/Promo.js
+++ src/containers/Promo.js
@@ -8,22 +8,48 @@ import {
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+} from 'native-base';
+
+import { tabChanged } from '../actions/application';
+import TitleBar from '../components/TitleBar';
+import BottomNav from '../components/BottomNav';
+
 export class Promo extends Component {
   constructor() {
     super();
   }
 
   render () {
+    const { tab, tabChanged } = this.props;
+
     return (
-      <View>
-        <Text>Promo</Text>
-      </View>
+      <Container>
+        <Header>
+          <Content>
+            <TitleBar tab={tab} />
+          </Content>
+        </Header>
+
+        <Content>
+          <Text>Promo</Text>
+        </Content>
+
+        <Footer>
+          <BottomNav tab={tab} tabChanged={tabChanged} />
+        </Footer>
+      </Container>
     );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
+    tab: state.application.tab,
   }
 }
 
@@ -32,5 +58,5 @@ Promo.propTypes = {
 
 export default connect(
   (state) => (mapStateToProps),
-  (dispatch) => bindActionCreators({/** _INSERT_ACTION_CREATORS_ **/}, dispatch)
+  (dispatch) => bindActionCreators({tabChanged}, dispatch)
 )(Promo);
