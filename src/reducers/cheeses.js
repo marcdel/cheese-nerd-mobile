99d93a7efec6f4527062b885b3420d9b0454b7f7
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-10-31 22:30:48 -0700

    Increment review count.

--- src/reducers/cheeses.js
+++ src/reducers/cheeses.js
@@ -1,8 +1,18 @@
 /* @flow */
+import * as reviewActions from '../actions/reviews'
+
 const INITIAL_STATE = require('../sample-cheeses');
 
 export default (state=INITIAL_STATE, action) => {
   switch (action.type) {
+    case reviewActions.REVIEW_ADDED:
+      return {
+        ...state,
+        [action.cheeseId]: {
+          ...state[action.cheeseId],
+          reviewCount: state[action.cheeseId].reviewCount + 1
+        }
+      };
     default:
       return state;
   }
--- src/sample-cheeses.js
+++ src/sample-cheeses.js
@@ -4,69 +4,79 @@ module.exports = {
     name : 'Gouda',
     image : 'http://www.cheese.com/media/img/cheese/Midnight_Moon_Gouda.jpg',
     desc : 'A semi-hard cheese celebrated for its rich, unique flavour and smooth texture.',
-    rating : 5
+    rating : 5,
+    reviewCount: 10,
   },
   cheese2 : {
     id: 'cheese2',
     name : 'Brie',
     image : 'http://www.cheese.com/media/img/cheese/Brie.jpg',
     desc : 'Brie is a soft cheese named after the French region Brie, where it was originally created.',
-    rating : 5
+    rating : 5,
+    reviewCount: 5,
   },
   cheese3 : {
     id: 'cheese3',
     name : 'Pecorino Romano',
     image : 'http://www.cheese.com/media/img/cheese/Pecorino_romano.jpg',
     desc : 'Pecorino Romano is one of most widely used, sharper alternatives to Parmesan cheeses.',
-    rating : 2
+    rating : 2,
+    reviewCount: 7,
   },
   cheese4 : {
     id: 'cheese4',
     name : 'Cheddar',
     image : 'http://www.cheese.com/media/img/cheese/wiki/cheddar.jpg',
     desc : 'A hard and natural cheese that has a slightly crumbly texture if properly cured and if it is too young, the texture is smooth.',
-    rating : 4
+    rating : 4,
+    reviewCount: 1,
   },
   cheese5 : {
     id: 'cheese5',
     name : 'Manchego',
     image : 'http://www.cheese.com/media/img/cheese/Manchego.jpg',
     desc : 'Made from unpasteurised sheep\'s milk, it is one of the popular cheeses from Spain.',
-    rating : 4
+    rating : 4,
+    reviewCount: 1,
   },
   cheese6 : {
     id: 'cheese6',
     name : 'Camembert',
     image : 'http://www.cheese.com/media/img/cheese/camembert_france.jpg',
     desc : 'Young Camembert has a milky and sweet taste. As the cheese matures it forms a smooth, runny interior and a white bloomy rind that is typical to Camenbert cheese.',
-    rating : 4
+    rating : 4,
+    reviewCount: 1,
   },
   cheese7 : {
     id: 'cheese7',
     name : 'Provolone',
     image : 'http://www.cheese.com/media/img/cheese/Provolone_3.jpg',
     desc : 'Provolone is an Italian cheese made from cow’s milk whose origins lie in Southern Italy.',
-    rating : 3
+    rating : 3,
+    reviewCount: 1,
   },
   cheese8 : {
     id: 'cheese8',
     name : 'Parmesan',
     image : 'http://www.cheese.com/media/img/cheese/Parmesan.JPG',
     desc : 'The Parmigiano Reggiano or Parmesan cheese as it is called in English is considered to be among the top cheeses by cheese connoisseurs.',
-    rating : 2
+    rating : 2,
+    reviewCount: 1,
   },
   cheese9 : {
     id: 'cheese9',
     name : 'Feta',
     image : 'http://www.cheese.com/media/img/cheese/Greek_Feta_1.jpg',
     desc : 'To create traditional feta, 30 percent goat\'s milk is mixed with sheep\'s milk of animals grazing on pastures in the specific appellation of origin regions.',
-    rating : 4
+    rating : 4,
+    reviewCount: 1,
   },
   cheese10 : {
     id: 'cheese10',
     name : 'Gruyere',
     image : 'http://www.cheese.com/media/img/cheese/gruyere_switzerland.jpg',
     desc : 'Gruyere is named after a Swiss village. It is traditional, creamery, unpasteurised, semi-soft cheese.',
-    rating : 2
+    rating : 2,
+    reviewCount: 1,
   }
 };
