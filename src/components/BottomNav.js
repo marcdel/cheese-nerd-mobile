4ae4515634415e7a05ceef50edf50b3a7374c980
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-11-08 16:52:07 -0800

    Override default theme inline.

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -11,7 +11,7 @@ import {
   Icon
 } from 'native-base';
 
-import theme from '../../Themes/cheese';
+import theme from '../../Themes/light';
 
 export default class BottomNav extends Component {
   constructor() {
@@ -32,9 +32,13 @@ export default class BottomNav extends Component {
 
   render () {
     const tab = this.props.tab;
+    const customTheme = {
+      ...theme,
+      tabActiveBgColor: 'transparent',
+    };
 
     return (
-      <FooterTab theme={theme}>
+      <FooterTab theme={customTheme}>
         {this.button('myBoard', 'My Board', 'ios-restaurant')}
         {this.button('add', 'Add', 'ios-add-circle')}
         {this.button('account', 'Account', 'ios-person')}
